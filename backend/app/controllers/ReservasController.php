a4e675c35015c4a6623ef1865992e62de385a08c
João Paulo Paes <joaopaulopaez@gmail.com>
2017-05-31 22:45:55 -0300

    Correção de bug no DER

--- backend/app/controllers/ReservasController.php
+++ backend/app/controllers/ReservasController.php
@@ -1,45 +1,69 @@
-<?php defined('INITIALIZED') OR exit('You cannot access this file directly');
+<?php
+
+defined('INITIALIZED') OR exit('You cannot access this file directly');
 
 class ReservasController extends Controller {
 
-	public function reservas($param){
-		switch( strtolower($this->getRequest()) ){
-			case 'get':
-				$this->get($param['id']);
-			break;
-			case 'post':
-				$this->post();
-			break;
-			case 'put':
-				$this->put($param['id']);
-			break;
-			case 'delete':
-				$this->delete($param['id']);
-			break;
-		}
-	}
+    public function reservas($param) {
+        switch (strtolower($this->getRequest())) {
+            case 'get':
+                $this->get(@$param['id']);
+                break;
+            case 'post':
+                $this->post();
+                break;
+            case 'put':
+                $this->put(@$param['id']);
+                break;
+            case 'delete':
+                $this->delete(@$param['id']);
+                break;
+        }
+    }
 
-	public function post(){
-		$reserva = new Reserva();
-		
-		$retorno['return'] = [
-			"type" => "success",
-			"message" => "Criado com sucesso!"
-		];
+    public function post() {
+        
+        $reserva = new Reserva();
+        $reserva->setAtivo(true);
+        $reserva->setDataHoraReserva($_POST['dataHoraReserva']);
+        $reserva->setHorario($_POST['horario']);
+        $reserva->setProfessor($_POST['professor']);
+        $reserva->setSala($_POST['sala']);
+        $res = $reserva->save();
 
-		echo json_encode($retorno);
-	}
+        if (is_null($res)) {
+            $retorno['return'] = [
+                "type" => "error",
+                "message" => "Não foi possível registrar a reserva.",
+                "object" => $res
+            ];
+        } else {
+            //TODO: gravar Log
+            $retorno['return'] = [
+                "type" => "success",
+                "message" => "Reserva registrada com sucesso"
+            ];
+        }
 
-	public function get($id = null){
-		echo json_encode(["return" => "Aqui mostra o registro de id " . $id]);
-	}
+        echo json_encode($retorno);
+    }
 
-	public function put($id = null){
-		echo json_encode(["return" => "Aqui atualiza o registro de id " . $id]);
-	}
+    public function get($id) {
+        if (is_null($id)) {
+            //Listar uma coleção
+            echo json_encode(["return" => "Todas as reservas " . $id]);
+        } else {
+            //Exibir detalhes de uma reserva
+            echo json_encode(["return" => "Aqui mostra o registro de id " . $id]);
+        }
+    }
 
-	public function delete($id = null){
-		echo json_encode(["return" => "Aqui exclui o registro de id " . $id]);
-	}
+    public function put($id = null) {
+        echo json_encode(["return" => "Aqui atualiza o registro de id " . $id]);
+    }
+
+    public function delete($id = null) {
+        echo json_encode(["return" => "Aqui exclui o registro de id " . $id]);
+    }
 
 }
