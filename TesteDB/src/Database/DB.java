df9d0eb271f8470c8511045837b41a7c10c6101b
Vinicius Baroni Soares <vinibaronisoares@gmail.com>
2017-06-08 17:34:08 -0300

    Removidos comentários de créditos gerados pelo netbeans

--- TesteDB/src/Database/DB.java
+++ TesteDB/src/Database/DB.java
@@ -7,16 +7,8 @@ import java.sql.SQLException;
 import Model.*;
 import java.sql.PreparedStatement;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author vinicius.baroni
- */
+
 public class DB {
     String host = "localhost";
     String database = "salasutf";
--- TesteDB/src/Main/Teste.java
+++ TesteDB/src/Main/Teste.java
@@ -8,16 +8,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author vinicius.baroni
- */
 public class Teste {
     public static void main(String[] args){
         
