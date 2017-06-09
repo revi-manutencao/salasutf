8fb85a1037de7c5ea9b7f947895c6d4d3a9575fe
Vinicius Baroni <vsoares@alunos.utfpr.edu.br>
2017-06-09 08:12:57 -0300

    Umas coisinhas aí, nem sei

---
+++ ModeloSistema/src/Main/Teste.java
@@ -0,0 +1,62 @@
+package Main;
+
+
+import Database.*;
+import Model.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+
+
+public class Teste {
+    public static void main(String[] args){
+        
+        BlocoDao bdao = new BlocoDao();
+        
+//        Bloco b = new Bloco();
+//        b.setCodigo("I");
+//        b.setAtivo(true);
+        
+//        Bloco blocoSearch = new Bloco();
+//        blocoSearch.setId(3);
+//        blocoSearch.setCodigo("I");
+//        Bloco bl = bdao.get(blocoSearch, "codigo");
+        
+
+
+        /**
+         * Teste de operações com o BD
+         */
+
+        //Insert
+//        bdao.insert(b);
+        
+        // Select all
+//        List<Bloco> blocos = bdao.getAll();        
+//        for (Bloco bloco : blocos){
+//            System.out.println("Cod: "+bloco.getCodigo()+"\nId: "+bloco.getId()+"\n");
+//        }
+        
+        // Select all actives
+//        List<Bloco> blocos = bdao.getAllAtivos();        
+//        for (Bloco bloco : blocos){
+//            System.out.println("Cod: "+bloco.getCodigo()+"\nId: "+bloco.getId()+"\n");
+//        }
+
+//        // Select specific
+//        System.out.println("Cod: "+bl.getCodigo()+"\nId: "+bl.getId());
+
+        // Disable object
+//        bdao.disable(bl);
+
+
+        // re enable object
+//        bdao.enable(bl);
+
+        // update object
+//        bl.setCodigo("K");
+//        bdao.update(bl);
+        
+    }
+}
