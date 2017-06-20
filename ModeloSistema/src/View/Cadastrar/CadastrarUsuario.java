3f611ed8d984586fe163790fd9936c921cefb2f5
Vinicius Baroni Soares <vinibaronisoares@gmail.com>
2017-06-20 13:32:03 -0300

    Ajustada a filtragem de funções de gerenciar pelos tipos de usuário

--- ModeloSistema/src/View/Cadastrar/CadastrarUsuario.java
+++ ModeloSistema/src/View/Cadastrar/CadastrarUsuario.java
@@ -8,7 +8,9 @@ package View.Cadastrar;
 import Database.DepartamentoDao;
 import Database.UsuarioDao;
 import Model.Departamento;
+import Model.TipoUsuario;
 import Model.Usuario;
+import Util.Auth;
 import Util.Hash;
 import static Util.Utility.disposeModal;
 import static Util.Utility.now;
@@ -94,7 +96,11 @@ public class CadastrarUsuario extends javax.swing.JPanel {
             }
         });
 
-        jcbTiposUsuário.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Professor", "Administrador" }));
+        if(Auth.getLoggedUser().getTipoUsuario() == TipoUsuario.ADMIN)
+        jcbTiposUsuário.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Professor" }));
+        else if(Auth.getLoggedUser().getTipoUsuario() == TipoUsuario.MASTER){
+            jcbTiposUsuário.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Professor", "Administrador" }));
+        }
         jcbTiposUsuário.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jcbTiposUsuárioActionPerformed(evt);
@@ -182,7 +188,7 @@ public class CadastrarUsuario extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jlEmail2)
                     .addComponent(jcbDepartamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbCadastrar)
                     .addComponent(jbCancelar))
